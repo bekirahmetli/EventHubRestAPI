63aca395f8dddcdb7dbd001e701f287eaba04a3a
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-12 22:13:12 +0300

    feat(entity): add Category entity with id and unique name fields

---
+++ src/main/java/com/example/entities/Category.java
@@ -0,0 +1,21 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long id;
+
+    @Column(name = "name",unique = true,nullable = false)
+    private String name;
+}
