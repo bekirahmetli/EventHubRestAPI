d0d68e139ce77eb5fddd662e4d14912e5f67159a
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-12 22:11:08 +0300

    feat(entity): create User entity with authProvider, role and basic user fields

---
+++ src/main/java/com/example/entities/User.java
@@ -0,0 +1,42 @@
+package com.example.entities;
+
+import com.example.enums.AuthProvider;
+import com.example.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email", unique = true,nullable = false)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private AuthProvider authProvider;
+
+    @Column(name = "avatar_url")
+    private String avatarUrl;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+
+
+}
