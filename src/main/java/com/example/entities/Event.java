e733eda48624f0881a53b37e0dd8b6b3f5894a07
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-12 22:14:41 +0300

    feat(entity): create Event entity with title, description, posterUrl, location and date fields

---
+++ src/main/java/com/example/entities/Event.java
@@ -0,0 +1,34 @@
+package com.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "events")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "event_id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description",columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "poster_url")
+    private String posterUrl;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "date")
+    private LocalDateTime date;
+}
