e9da8173acae88aba4a0893e1c848e2ce622c6bc
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-13 20:16:58 +0300

    feat(entity): add one-to-many relationship between Event and Registration

--- src/main/java/com/example/entities/Event.java
+++ src/main/java/com/example/entities/Event.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "events")
@@ -31,4 +32,7 @@ public class Event {
 
     @Column(name = "date")
     private LocalDateTime date;
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Registration> registrations;
 }
