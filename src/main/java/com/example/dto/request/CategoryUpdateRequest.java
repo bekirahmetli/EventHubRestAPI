6c50477c6ae9f9e1b1e8bb5d5031ace491bfd157
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-14 20:02:13 +0300

    feat(dto): add CategoryUpdateRequest DTO with validation rules

---
+++ src/main/java/com/example/dto/request/CategoryUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.example.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Kategori güncelleme işlemi için kullanılan request DTO’su. Güncellenecek kategori ID’si ve yeni kategori adını içerir.
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryUpdateRequest {
+    @NotNull(message = "ID boş olamaz")
+    private Long id;
+
+    @NotBlank(message = "Kategori adı boş olamaz")
+    private String name;
+}
