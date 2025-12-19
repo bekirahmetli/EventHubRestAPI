00ee46c72aeaa358b47bb26fec9dd903d8af5682
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-19 17:03:54 +0300

    feat(result): add ResultHelper utility for standardized created responses

---
+++ src/main/java/com/example/result/ResultHelper.java
@@ -0,0 +1,22 @@
+package com.example.result;
+
+/**
+ * API response’ları için hazır Result ve ResultData nesneleri
+ * oluşturmaya yardımcı olan helper sınıfı.
+ *
+ * Amaç:
+ * - Controller katmanında tekrar eden response oluşturma kodlarını azaltmak
+ * - Standart ve tutarlı API cevapları üretmek
+ */
+public class ResultHelper {
+    /**
+     * Başarılı bir POST işlemi sonrası kullanılan response.
+     *
+     * @param data Oluşturulan ve client’a döndürülecek veri
+     * @param <T>  Döndürülecek veri tipi
+     * @return     201 status kodu ile oluşturulmuş ResultData
+     */
+    public static <T> ResultData <T> created(T data){
+        return new ResultData<>(true,"Kayıt eklendi","201",data);
+    }
+}
