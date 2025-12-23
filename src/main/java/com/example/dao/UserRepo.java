f2bf8ac669c5d259037eb21070106cb37be64e9f
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-23 23:44:42 +0300

    feat(repository): add findByEmail method to UserRepo

--- src/main/java/com/example/dao/UserRepo.java
+++ src/main/java/com/example/dao/UserRepo.java
@@ -3,8 +3,10 @@ package com.example.dao;
 import com.example.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepo extends JpaRepository<User,Long> {
     boolean existsByEmail(String email); // Email'in veritabanında mevcut olup olmadığını kontrol eder
+    Optional<User> findByEmail(String email); // Email ile kullanıcı bulma
 }
