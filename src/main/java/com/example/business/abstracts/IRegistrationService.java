ffdad330ead475dc14998005cd7c5248c5c2bc9b
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-31 22:29:17 +0300

    feat(service): add cursor method to IRegistrationService

--- src/main/java/com/example/business/abstracts/IRegistrationService.java
+++ src/main/java/com/example/business/abstracts/IRegistrationService.java
@@ -1,10 +1,12 @@
 package com.example.business.abstracts;
 
 import com.example.entities.Registration;
+import org.springframework.data.domain.Page;
 
 public interface IRegistrationService {
     //Registration entity için temel CRUD (Create, Read, Update, Delete) ve listeleme işlemleri
     Registration save(Registration registration);
     Registration get(Long id);
     Registration update(Registration registration);
+    Page<Registration> cursor(int page, int pageSize);
 }
