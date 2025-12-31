4cd5974cc6f08d4fdace380bc6518ccd7238993c
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-31 22:26:51 +0300

    feat(service): add save method to IRegistrationService

--- src/main/java/com/example/business/abstracts/IRegistrationService.java
+++ src/main/java/com/example/business/abstracts/IRegistrationService.java
@@ -1,4 +1,8 @@
 package com.example.business.abstracts;
 
+import com.example.entities.Registration;
+
 public interface IRegistrationService {
+    //Registration entity için temel CRUD (Create, Read, Update, Delete) ve listeleme işlemleri
+    Registration save(Registration registration);
 }
