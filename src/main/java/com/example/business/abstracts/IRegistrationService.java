6c6a7f42a176ad186d5dc1a01bfe943245b09c99
bekirahmetli <bekirahmetli1905@gmail.com>
2025-12-31 22:27:36 +0300

    feat(service): add get method to IRegistrationService

--- src/main/java/com/example/business/abstracts/IRegistrationService.java
+++ src/main/java/com/example/business/abstracts/IRegistrationService.java
@@ -5,4 +5,5 @@ import com.example.entities.Registration;
 public interface IRegistrationService {
     //Registration entity için temel CRUD (Create, Read, Update, Delete) ve listeleme işlemleri
     Registration save(Registration registration);
+    Registration get(Long id);
 }
