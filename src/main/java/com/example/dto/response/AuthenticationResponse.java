e25eec3954fb50cf9c28f3c41b6277811f46d4eb
bekirahmetli <bekirahmetli1905@gmail.com>
2026-01-02 22:02:30 +0300

    refactor(auth): add builder support to AuthenticationResponse DTO

--- src/main/java/com/example/dto/response/AuthenticationResponse.java
+++ src/main/java/com/example/dto/response/AuthenticationResponse.java
@@ -1,6 +1,7 @@
 package com.example.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class AuthenticationResponse {
     private String token; // JWT token
     private String tokenType = "Bearer"; // Token tipi
