fe563c49d5f9fa8ab631978bb6aeb506d2daee86
bekirahmetli <bekirahmetli1905@gmail.com>
2026-01-03 21:52:58 +0300

    feat(auth): add oauth2Login method to authentication service

--- src/main/java/com/example/business/abstracts/IAuthenticationService.java
+++ src/main/java/com/example/business/abstracts/IAuthenticationService.java
@@ -4,6 +4,7 @@ import com.example.dto.request.auth.LoginRequest;
 import com.example.dto.request.auth.RefreshTokenRequest;
 import com.example.dto.request.auth.RegisterRequest;
 import com.example.dto.response.AuthenticationResponse;
+import com.example.enums.AuthProvider;
 
 //Authentication (giriş/kayıt) işlemlerini yöneten servis interface'i
 public interface IAuthenticationService {
@@ -11,4 +12,5 @@ public interface IAuthenticationService {
     AuthenticationResponse login(LoginRequest request); // Kullanıcı girişi
     AuthenticationResponse refreshToken(RefreshTokenRequest request);// Refresh token kullanarak yeni bir access token üretir.
     void logout(String refreshToken); // Refresh token'ı geçersiz kılar
+    AuthenticationResponse oauth2Login(String email, String name, AuthProvider provider, String avatarUrl);// OAuth2 için
 }
